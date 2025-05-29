f022adbffb6e55c4d20c6c1fd5abcec85d347b27
Wenxi Jin <2374124+WenxiJin@users.noreply.github.com>
2025-05-29 23:17:43 +0200

    Add gruvbox colortheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  lazy = false,
+  config = function()
+    require("gruvbox").setup({
+      contrast = "soft",
+      palette_overrides = {
+        -- Customize colors if you want
+      },
+    })
+    vim.o.background = "light" -- Set light background
+    vim.cmd([[colorscheme gruvbox]])
+  end,
+}
