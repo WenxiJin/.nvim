f59147d597148884b3de77f519b2ef7e9a5324ee
Wenxi Jin <2374124+WenxiJin@users.noreply.github.com>
2025-09-18 15:16:57 +0200

    Change tab to 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,9 @@
 -- Add any additional options here
 --
 vim.g.autoformat=false
-vim.opt.shiftwidth=4
+
+local opt = vim.opt
+
+opt.tabstop = 4       -- A TAB character looks like 4 spaces
+opt.shiftwidth = 4    -- Indents will be 4 spaces
+opt.expandtab = true  -- Pressing the TAB key inserts spaces instead of a TAB character
